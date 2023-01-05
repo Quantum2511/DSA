2680eb1f010b72f86448b78e71aed451ccda80b9
Bhavya Chanana <65809399+Quantum2511@users.noreply.github.com>
2023-01-06 00:56:10 +0530

    optimzed

--- bubbleSort.py
+++ bubbleSort.py
@@ -2,12 +2,16 @@
 # Worst case: O(n^2)
 
 def bubbleSort(arr):
+    swapped = False
     for i in range(len(arr) - 1):
+
         for j in range(len(arr) - i - 1):
+        
             if arr[j] > arr[j + 1]:
+                swapped = True
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-    return arr
-
+    
+    if not swapped: return 
+    
 arr = [3, 2, 5, 4, 1]
 print(bubbleSort(arr))
