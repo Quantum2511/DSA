9e2d209d32ffcae9e8471706cc936484457a0014
Bhavya Chanana <65809399+Quantum2511@users.noreply.github.com>
2023-01-06 00:49:07 +0530

    optimized bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -2,11 +2,10 @@
 # Worst case: O(n^2)
 
 def bubbleSort(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr) - 1):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-        print(arr)
 
     return arr
 
